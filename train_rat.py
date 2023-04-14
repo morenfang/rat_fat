9b0d246ab35ae639f19e8ef64a837d1da98f2ae4
方智毅_Zhiyi <zhiyi.fang@enflame.cn>
2023-04-14 16:07:12 +0800

    remove redundant import

--- train_rat.py
+++ train_rat.py
@@ -1,8 +1,7 @@
 from process_data import *
 import torch
 import torch.nn.functional as F
-import numpy as np
-import composition_stats as coms
+
 
 train_data, labels = get_data('./data/fat_data.xlsx')
 train_data = torch.from_numpy(train_data)
